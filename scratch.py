5aa070d0c74ed60e33fb2bf913a145a08edcfdb8
Edmond Wen <EdmondWen@MacBook-Air-4.local>
2019-04-02 13:28:47 -0700

    Rosetta stone needs fixing

--- scratch.py
+++ scratch.py
@@ -56,6 +56,20 @@ class GameBoard:
         "Return true if the move is in vertical orientation."
         return not self.isHorizontal(self, move)
 
+    def rosettaStoneIndex(self, move):
+        num = 0
+        for(x1 in range(self.width)):
+            for(y1 in range(self.height)):
+                for(x2 in range(self.width)):
+                    for(y2 in range(self.height)):
+                        if(ultimateCheck(((x1,y1), (x2,y2)))):
+                            num = num + 1
+                        if (move == ((x1,y1), (x2,y2))):
+                            return num
+
+    def rosettaStoneCoord(self, move):
+        
+
     def play(self, move):
         """Place a particular move on the board.  If any wackiness
         occurs, raise an AssertionError.  Returns a list of
